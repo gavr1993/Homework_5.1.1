b9195e1887e439cd7de0a89c4f9fb81b5067c506
GAVR <axotnick@bk.ru>
2020-05-28 12:24:54 +0300

    save to file added

--- app/src/main/java/com/example/homework_411/ListViewActivity.java
+++ app/src/main/java/com/example/homework_411/ListViewActivity.java
@@ -1,18 +1,23 @@
 package com.example.homework_411;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,68 +27,133 @@ public class ListViewActivity extends AppCompatActivity {
 
     private static final String KEY_TEXT = "key_text";
     private static final String KEY_COUNT = "key_count";
-    private static final String NOTE_TXT = "note_txt";
-    private SwipeRefreshLayout swipeRefresh;
-    private SharedPreferences textSharedPref;
-    List<Map<String, String>> simpleAdapterContent = new ArrayList<>();
+    private static final String STORAGE_FILE = "storage_file.txt";
+
+    private List<Map<String, String>> values;
+
+    private BaseAdapter listContentAdapter;
+    final ListView list = findViewById(R.id.list);
+
+    private ListViewActivity() {
+        listContentAdapter = null;
+    }
+
+    private ImageButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-        final BaseAdapter listContentAdapter = createAdapter(simpleAdapterContent);
-        final ListView list = findViewById(R.id.list);
+
+        values = prepareContent();
+        listContentAdapter = createAdapter(values);
         list.setAdapter(listContentAdapter);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                simpleAdapterContent.remove(position);
-                listContentAdapter.notifyDataSetChanged();
-            }
-        });
-
-        textSharedPref = getSharedPreferences("my_prefs", MODE_PRIVATE);
-        saveDateToSharedPref();
-        loadDateFromSharedPref();
-        listContentAdapter.notifyDataSetChanged();
-        swipeRefresh = findViewById(R.id.swipeRefresh);
-        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        simpleAdapterContent.clear();
-                        listContentAdapter.notifyDataSetChanged();
-                        loadDateFromSharedPref();
-                        listContentAdapter.notifyDataSetChanged();
-                        swipeRefresh.setRefreshing(false);
-                    }
-                }, 100);
-            }
-        });
+        add = findViewById(R.id.add);
+        addItem();
     }
 
-    private void loadDateFromSharedPref() {
-        String text = textSharedPref.getString(NOTE_TXT, "");
-        final String[] titles = text.split("\n\n");
-        for (String title : titles) {
-            Map<String, String> map = new HashMap<>();
-            map.put(KEY_TEXT, title);
-            map.put(KEY_COUNT, title.length() + "");
-            simpleAdapterContent.add(map);
-        }
-    }
+    private void addItem() {
+        EditText edtx = findViewById(R.id.edtx);
+        String author = String.valueOf(edtx.getText());
+        
+           }
 
-    private void saveDateToSharedPref() {
-        if (textSharedPref.getString(NOTE_TXT, "").isEmpty()) {
-            textSharedPref.edit().putString(NOTE_TXT, getString(R.string.large_text)).apply();
-        }
+    private File getValuesFile() {
+        return new File(getExternalFilesDir(null), STORAGE_FILE);
     }
 
     @NonNull
     private BaseAdapter createAdapter(List<Map<String, String>> text) {
         return new SimpleAdapter(this, text, R.layout.double_text, new String[]
-                {KEY_TEXT, KEY_COUNT}, new int[]{R.id.textViewTop, R.id.textViewBottom});
+                {KEY_TEXT, KEY_COUNT}, new int[]{R.id.textViewTop, R.id.textViewBottom}) {
+            @Override
+            public View getView(final int position, View convertView, ViewGroup parent) {
+                View v = super.getView(position, convertView, parent);
+                ImageButton del = v.findViewById(R.id.del);
+                del.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        values.remove(position);
+                        listContentAdapter.notifyDataSetChanged();
+                    }
+                });
+                return v;
+            }
+        };
+    }
+
+    @NonNull
+    private List<Map<String, String>> prepareContent() {
+        List<Map<String, String>> result = new ArrayList<>();
+        File file = getValuesFile();
+        if (file.exists()) {
+            return loadFromFile(file, KEY_TEXT, KEY_COUNT);
+        } else {
+            String[] titles = getString(R.string.large_text).split("\n\n");
+            for (String title : titles) {
+                Map<String, String> map = new HashMap<>();
+                map.put(KEY_TEXT, title);
+                map.put(KEY_COUNT, title.length() + "");
+                result.add(map);
+            }
+            saveList(result, KEY_TEXT, KEY_COUNT, file);
+        }
+        return result;
+    }
+
+    public static void saveList(List<Map<String, String>> data, String key1, String key2, File file) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            for (Map<String, String> map : data) {
+                String text = map.get(key1);
+                String countText = map.get(key2);
+                writer.write(text + ";" + countText);
+                writer.write("\n\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static List<Map<String, String>> loadFromFile(File file, String key1, String key2) {
+        List<Map<String, String>> result = new ArrayList<>();
+        BufferedReader reader = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            int symbol;
+            while ((symbol = reader.read()) != -1) {
+                sb.append((char) symbol);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        String fullFileContent = sb.toString();
+        String[] titles = fullFileContent.split("\n\n");
+        for (String title : titles) {
+            String[] values = title.split(";");
+            Map<String, String> map = new HashMap<>();
+            map.put(key1, values[0]);
+            map.put(key2, values[1]);
+            result.add(map);
+        }
+        return result;
     }
 }
